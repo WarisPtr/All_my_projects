2aa2899d1b14bed82076de79594ba6f8fb630ad7
Ham <hamwaris.ptr@gmail.com>
2025-07-03 00:21:29 +0700

    edit single page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import '../styles/Navbar.css'
 const Menu =()=>{
   return(
     <header className="header-wrapper">
-      <Link to="/" className="flex w-[30%] items-center">
+      <Link to="/" className="logo-container">
         <img src="/code.png" alt="logo-img" className="logo-img"/>
         <div id="logo" className="logo">
           <h1>MY LOGO</h1>
@@ -14,7 +14,7 @@ const Menu =()=>{
         <Link className="nav-item" to="/">หน้าแรก</Link>
         {/* <Link className="nav-item" to="/About">เกี่ยวกับเรา</Link> */}
         <Link className="nav-item" to="/myproject">โปรเจก</Link>
-        <Link className="nav-item" to="/Contact">ติดต่อเรา</Link>
+        <Link className="nav-item" to="/Contact">ติดต่อ</Link>
       </nav>
     </header>
   )
