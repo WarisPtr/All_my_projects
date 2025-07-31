228c26b4b348e315e1e2402b50b7d1288e8a3c4f
Ham <hamwaris.ptr@gmail.com>
2025-07-31 18:38:13 +0700

    responsive

--- src/project/Wordpress/sigmasolutions.jsx
+++ src/project/Wordpress/sigmasolutions.jsx
@@ -89,7 +89,7 @@ export default function Sigma() {
   return (
     <div>
       {/* Gallery Grid */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 max-w-6xl mx-auto p-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4  mx-auto p-4">
         {images.map((image, index) => (
           <div
             key={image.id}
